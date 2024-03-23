191a2baa5974dfa4377fcc0d12620acb502ea883
Darryl Yeo <dev@darryl-yeo.com>
2024-03-22 23:31:10 -0700

    Layout: render frame meta from SvelteKit page data

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,17 @@
+import type { FrameMeta } from './lib/frame'
+
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
-		// interface PageData {}
+
+		interface PageData {
+			frame?: Omit<FrameMeta, 'image'>,
+		}
+
 		// interface PageState {}
 		// interface Platform {}
 	}
