0b71f6c6307ab82698a3c62807e3156e072dad0d
Darryl Yeo <dev@darryl-yeo.com>
2024-03-22 20:57:30 -0700

    Farcaster Frames: model types

---
+++ src/lib/farcaster.ts
@@ -0,0 +1,5 @@
+export type FarcasterUserId = number
+export type FarcasterUserName = string
+export type FarcasterCastId = `0x${string}`
+export type FarcasterCastShortId = `0x${string}`
+export type FarcasterChannelSlug = string
