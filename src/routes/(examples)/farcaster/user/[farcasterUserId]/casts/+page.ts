c65ed8160d0b3015d5339190745e63ae1f160822
Darryl Yeo <dev@darryl-yeo.com>
2024-03-24 03:31:30 -0700

    Farcaster User: fetch and display recent casts from Pinata

--- src/routes/(examples)/farcaster/user/[farcasterUserId]/casts/+page.ts
+++ src/routes/(examples)/farcaster/user/[farcasterUserId]/casts/+page.ts
@@ -1,26 +1,21 @@
-// Tyoes
-import type { FrameMeta } from '$/lib/frame'
-
-
-// Data
 import type { PageLoad } from './$types'
+import type { FrameMeta } from '$/lib/frame'
 
 export const load: PageLoad = async ({
 	parent,
+	data,
 }) => {
 	const parentData = await parent()
 
 	return {
-		...parentData,
+		...data,
 
 		frame: {
+			image: {
+				aspectRatio: '1:1',
+			},
 			buttons: [
 				parentData.frame.buttons[0],
-				{
-					label: 'Casts',
-					action: 'post',
-					targetUrl: './casts',
-				},
 			],
 		} as FrameMeta,
 	}
