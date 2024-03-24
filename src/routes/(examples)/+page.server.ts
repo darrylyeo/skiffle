ef7c28a62ed5328a8e9b10e6e1456b1657c7384e
Darryl Yeo <dev@darryl-yeo.com>
2024-03-24 08:17:01 -0700

    Example Demos: move to menu with pagination

--- src/routes/(examples)/+page.server.ts
+++ src/routes/(examples)/+page.server.ts
@@ -8,10 +8,39 @@ import type { Actions } from './$types'
 
 export const actions: Actions = {
 	demos: async ({
+		url,
 		locals: { frameSignaturePacket },
 	}) => {
 		const farcasterUserId = frameSignaturePacket?.untrustedData.fid ?? 3
 
+		const currentPage = Number(url.searchParams.get('page') ?? 0)
+
+		const demos = [
+			{
+				label: 'Farcaster Users',
+				action: 'post',
+				targetUrl: resolveRoute(`/farcaster/user/[farcasterUserId]`, { farcasterUserId: String(farcasterUserId) }),
+			},
+			{
+				label: 'Top Frames',
+				action: 'post',
+				targetUrl: '/farcaster/frames',
+			},
+			{
+				label: 'Mint',
+				action: 'mint',
+				targetUrl: 'eip155:7777777:0x55f5a5d980992e01256d86e7ef03a22fd5fe84af',
+			},
+			{
+				label: 'Useless Tx',
+				action: 'tx',
+				targetUrl: '/tx',
+			},
+		]
+
+		const itemsPerPage = 2
+		const totalPages = Math.ceil(demos.length / itemsPerPage)
+		
 		return {
 			frame: {
 				image: {
@@ -24,15 +53,14 @@ export const actions: Actions = {
 						action: 'post',
 						targetUrl: '/',
 					},
+					...demos.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage),
 					{
-						label: 'Farcaster Users',
+						label: 'More Demos ›',
 						action: 'post',
-						targetUrl: resolveRoute(`/farcaster/user/[farcasterUserId]`, { farcasterUserId: String(farcasterUserId) }),
-					},
-					{
-						label: 'Top Frames',
-						action: 'post',
-						targetUrl: '/farcaster/frames',
+						targetUrl: `?${new URLSearchParams({
+							...Object.fromEntries(url.searchParams.entries()),
+							page: String((currentPage + 1) % totalPages),
+						})}`,
 					},
 				],
 			} as FrameMeta,
--- src/routes/(examples)/+page.ts
+++ src/routes/(examples)/+page.ts
@@ -10,16 +10,6 @@ export const load: PageLoad = async () => {
 					targetUrl: '/about',
 				},
 				{
-					label: 'Mint',
-					action: 'mint',
-					targetUrl: 'eip155:7777777:0x55f5a5d980992e01256d86e7ef03a22fd5fe84af',
-				},
-				{
-					label: 'Tx',
-					action: 'tx',
-					targetUrl: '/tx',
-				},
-				{
 					label: 'Demos...',
 					action: 'post',
 					targetUrl: '?/demos',
