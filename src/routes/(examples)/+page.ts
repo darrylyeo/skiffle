804403831f05f704ec5cdba7d15f67b5a679905c
Darryl Yeo <dev@darryl-yeo.com>
2024-03-23 21:34:41 -0700

    Example: update mint link

--- src/routes/(examples)/+page.ts
+++ src/routes/(examples)/+page.ts
@@ -12,7 +12,7 @@ export const load: PageLoad = async () => {
 				{
 					label: 'Mint',
 					action: 'mint',
-					targetUrl: '/mint',
+					targetUrl: 'eip155:7777777:0x55f5a5d980992e01256d86e7ef03a22fd5fe84af',
 				},
 				{
 					label: 'Tx',
