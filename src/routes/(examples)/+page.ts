19f463ed618216643c3f35fe28562350834e1e95
Darryl Yeo <dev@darryl-yeo.com>
2024-03-24 08:19:34 -0700

    Add project links

--- src/routes/(examples)/+page.ts
+++ src/routes/(examples)/+page.ts
@@ -10,6 +10,16 @@ export const load: PageLoad = async () => {
 					targetUrl: '/about',
 				},
 				{
+					label: 'GitHub',
+					action: 'link',
+					targetUrl: 'https://github.com/darrylyeo/skiffle',
+				},
+				{
+					label: 'Follow',
+					action: 'link',
+					targetUrl: 'https://warpcast.com/darrylyeo',
+				},
+				{
 					label: 'Demos...',
 					action: 'post',
 					targetUrl: '?/demos',
