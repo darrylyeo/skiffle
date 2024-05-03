4206bb93db0120dc81276f3849578c9077f0a215
Darryl Yeo <dev@darryl-yeo.com>
2024-05-03 13:26:24 -0700

    PageData: define `title` / `width` props

--- src/app.d.ts
+++ src/app.d.ts
@@ -16,6 +16,16 @@ declare global {
 
 		interface PageData {
 			/**
+			 * The title of the page that is being rendered as a frame.
+			 */
+			title?: string,
+
+			/**
+			 * The width of the frame image in pixels.
+			 */
+			width?: number,
+
+			/**
 			 * Farcaster frame metadata associated with this page, including image aspect ratio and buttons to render when this page's URL is embedded in a Farcaster cast.
 			 */
 			frame?: Omit<FrameMeta, 'image'> & { image?: Omit<FrameMeta['image'], 'url'> & { url?: string } },
